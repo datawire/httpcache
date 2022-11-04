bce013c68f0a6e4c10ad29f70e02036cde9d5d66
Luke Shumaker <lukeshu@datawire.io>
2022-11-04 03:43:48 -0600

    Don't skip tests

--- memcache/memcache_test.go
+++ memcache/memcache_test.go
@@ -15,8 +15,7 @@ const testServer = "localhost:11211"
 func TestMemCache(t *testing.T) {
 	conn, err := net.Dial("tcp", testServer)
 	if err != nil {
-		// TODO: rather than skip the test, fall back to a faked memcached server
-		t.Skipf("skipping test; no server running at %s", testServer)
+		t.Fatal(err)
 	}
 	conn.Write([]byte("flush_all\r\n")) // flush memcache
 	conn.Close()
--- redis/redis_test.go
+++ redis/redis_test.go
@@ -10,8 +10,7 @@ import (
 func TestRedisCache(t *testing.T) {
 	conn, err := redis.Dial("tcp", "localhost:6379")
 	if err != nil {
-		// TODO: rather than skip the test, fall back to a faked redis server
-		t.Skipf("skipping test; no server running at localhost:6379")
+		t.Fatal(err)
 	}
 	conn.Do("FLUSHALL")
 
